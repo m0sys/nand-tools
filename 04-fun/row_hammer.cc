1fd9ca85078934311fafe996c79d4147f9825bd4
mhd53 <mhd53@users.noreply.github.com>
2021-10-01 13:51:55 -0400

    feat: row_hammer second attempt

--- 04-fun/row_hammer.cc
+++ 04-fun/row_hammer.cc
@@ -1,21 +1,37 @@
 #include <iostream>
 #define LOG(x) std::cout << x << "\n"
 
+inline void clflush(volatile void *p)
+{
+    asm volatile ("clflush (%0)" :: "r"(p));
+}
+
+inline void mfence()
+{
+    asm volatile ("mfence" ::: "memory");
+}
+
 int main()
 {
     int* ptr = new int[10];
+    int delta = 4;
     int counter = 0;
     while (true) {
         if (counter % 2 == 0) {
 
-            *ptr = 0;
+            *ptr = 1;
+            *(ptr + delta) = 0;
             LOG(counter << ": hammering a " << *ptr);
             
         }
         else {
-            *ptr = 1;
+            *ptr = 0;
+            *(ptr + delta) = 1;
             LOG(counter << ": hammering a " << *ptr);
         }
+        clflush(ptr);
+        clflush(ptr+delta);
+        mfence();
         counter++;
     }
 }
