1c41b6e1c88689e090256e814272a150e7c6c30c
m0sys <m0sys@users.noreply.github.com>
2021-11-22 11:44:53 -0500

    feat(nt): add asm mult

--- 03-textbooks/02-nand2tetris/projects/04/mult/Mult.asm
+++ 03-textbooks/02-nand2tetris/projects/04/mult/Mult.asm
@@ -9,4 +9,55 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Even better than commented pseudo code is python!
+// i = 0
+// res = 0
+// LOOP:
+// 	if (i >= R0)
+// 		goto STOP
+// 	res += R1
+//  i++
+// STOP:
+// 	R2 = res
+
+// i = 0
+@i
+M=0
+
+// res = 0
+@res
+M=0
+
+(LOOP)
+	// if (i >= R0)
+	@i
+	D=M
+	@R0
+	D=D-M
+	@STOP
+	D;JGE
+	// res += R1
+	@res
+	D=M
+	@R1
+	D=D+M
+	@res
+	M=D
+	// i++
+	@i
+	M=M+1
+	// goto LOOP
+	@LOOP
+	0;JMP
+(STOP)
+	// R2 = res
+	@res
+	D=M
+	@R2
+	M=D
+(END)
+	@END
+	0;JMP
+
+
+
