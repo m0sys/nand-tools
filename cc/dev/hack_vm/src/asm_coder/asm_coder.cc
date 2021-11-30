bee083cb49621dc30607ecb76b2d25a88489c14e
m0sys <m0sys@users.noreply.github.com>
2021-11-30 10:59:07 -0500

    feat(nt): add write_pop impl - t-13h to aoc!

--- cc/dev/hack_vm/src/asm_coder/asm_coder.cc
+++ cc/dev/hack_vm/src/asm_coder/asm_coder.cc
@@ -18,6 +18,7 @@ void AsmCoder::write_push_pop(bool is_push, const std::string& seg, int i)
 
 void AsmCoder::close() { outfile.close(); }
 
+// Writes seg[i] to the top of the stack.
 void AsmCoder::write_push(const std::string& seg, int i)
 {
     // Figure out where the data comes from.
@@ -64,11 +65,60 @@ void AsmCoder::write_push(const std::string& seg, int i)
         outfile << "D=M\n";
     }
 
-    // Push onto stack step.
+    // Push onto stack.
     outfile << "@SP\n";
     outfile << "A=M\n";
     outfile << "M=D\n";
     outfile << "@SP\n";
     outfile << "M=M+1\n";
 }
-void AsmCoder::write_pop(const std::string& seg, int i) { }
+
+// Writes top of the stack to seg[i].
+void AsmCoder::write_pop(const std::string& seg, int i)
+{
+
+    // Pop top of the stack.
+    outfile << "@SP\n";
+    outfile << "A=M\n";
+    outfile << "M=M-1\n";
+    outfile << "@M\n";
+    outfile << "D=M\n";
+
+    // Figure out which register to write to.
+    if (seg == "local")
+        outfile << "@LCL\n";
+
+    else if (seg == "argument")
+        outfile << "@ARG\n";
+
+    else if (seg == "this")
+        outfile << "@THIS\n";
+
+    else if (seg == "that")
+        outfile << "@THAT\n";
+
+    // Store D register in appropriate register.
+
+    if (seg == "pointer") {
+        if (i == 0)
+            outfile << "@THIS\n";
+        else
+            outfile << "@THAT\n";
+
+        outfile << "M=D\n";
+    }
+
+    else if (seg == "temp") {
+        outfile << "@5\n";
+        outfile << "D=A\n";
+        outfile << "@" << std::to_string(i) << "\n";
+        outfile << "A=D+A\n";
+        outfile << "M=D\n";
+    }
+
+    else { // seg = {local|argument|this|that}
+        // Load (base+i) into D register.
+        outfile << "A=M+" << std::to_string(i) << "\n";
+        outfile << "M=D\n";
+    }
+}
