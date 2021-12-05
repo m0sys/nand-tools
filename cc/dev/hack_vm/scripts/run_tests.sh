4656b50674667d2fa40bc162e40f50f166895358
m0sys <m0sys@users.noreply.github.com>
2021-12-05 10:31:39 -0500

    refactor(nt): unify asm gen syntax

--- cc/dev/hack_vm/scripts/run_tests.sh
+++ cc/dev/hack_vm/scripts/run_tests.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 cmake --build build
-./build/hack_vm_test
+#./build/hack_vm_test
+
+printf "Boostrap code\n"
+./build/hack_vm ../../../projects/08/FunctionCalls/StaticsTest/
+./build/hack_vm ../../../projects/08/FunctionCalls/FibonacciElement/
+./build/hack_vm ../../../projects/08/FunctionCalls/NestedCall/
+
+
+printf "Non-Boostrap code\n"
+./build/hack_vm ../../../projects/08/FunctionCalls/SimpleFunction/SimpleFunction.vm
+./build/hack_vm ../../../projects/08/ProgramFlow/BasicLoop/BasicLoop.vm
+./build/hack_vm ../../../projects/08/ProgramFlow/FibonacciSeries/FibonacciSeries.vm
+
+printf "Done with Asm Generation!\n\n"
