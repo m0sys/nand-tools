a3384691560de365afab65e0491c4eaf4c965511
M0 <m0sys@users.noreply.github.com>
2022-03-24 07:02:26 -0400

    feat(csapp): add loop unrolling + elim mem ref optim

--- 03-textbooks/03-csapp/05-optim-prog/vec/vec.c
+++ 03-textbooks/03-csapp/05-optim-prog/vec/vec.c
@@ -70,3 +70,100 @@ void combine2(vec_ptr v, data_t* dest)
         *dest = *dest OP val;
     }
 }
+
+data_t* get_vec_start(vec_ptr v) { return v->data; }
+
+/* Elim Func Call: Direct access to vector data */
+void combine3(vec_ptr v, data_t* dest)
+{
+    long i;
+    long length = vec_length(v);
+    data_t* data = get_vec_start(v);
+
+    *dest = IDENT;
+    for (i = 0; i < length; i++) {
+        *dest = *dest OP data[i];
+    }
+}
+
+/* Elim Mem Ref: Accum res in local var */
+void combine4(vec_ptr v, data_t* dest)
+{
+    long i;
+    long length = vec_length(v);
+    data_t* data = get_vec_start(v);
+    data_t acc = IDENT;
+    for (i = 0; i < length; i++) {
+        acc = acc OP data[i];
+    }
+    *dest = acc;
+}
+
+/* Loop Unrolling (2x1 loop unrolling) */
+void combine5(vec_ptr v, data_t* dest)
+{
+    long i;
+    long length = vec_length(v);
+    long limit = length - 1;
+    data_t* data = get_vec_start(v);
+    data_t acc = IDENT;
+
+    /* Combine 2 elems at a time */
+    for (i = 0; i < limit; i += 2) {
+        acc = (acc OP data[i])OP data[i + 1];
+    }
+
+    /* Finish any remaining elems */
+    // This is code that is not good taste.
+    for (; i < length; i++)
+        acc = acc OP data[i];
+
+    *dest = acc;
+}
+
+/* Loop Unrolling (5x1 loop unrolling) */
+void combine5_5(vec_ptr v, data_t* dest)
+{
+    long i;
+    long length = vec_length(v);
+    long limit = length - 4; // n - k + 1 where k = 5
+    data_t* data = get_vec_start(v);
+    data_t acc = IDENT;
+
+    /* Combine 5 elems at a time */
+    for (i = 0; i < limit; i += 5) {
+        acc = (acc OP data[i])OP data[i + 1] OP data[i + 2] OP data[i + 3] OP data[i + 4];
+    }
+
+    /* Finish any remaining elems */
+    // This is code that is not good taste.
+    for (; i < length; i++)
+        acc = acc OP data[i];
+
+    *dest = acc;
+}
+
+/* 2 x 2 loop unrolling */
+void combine6(vec_ptr v, data_t* dest)
+{
+    long i;
+    long length = vec_length(v);
+    long limit = length - 1;
+    data_t* data = get_vec_start(v);
+    data_t acc0 = IDENT;
+    data_t acc1 = IDENT;
+
+    /* Combine 2 elements at a time */
+    for (i = 0; i < limit; i += 2) {
+        acc0 = acc0 OP data[i];
+        acc1 = acc1 OP data[i + 1];
+    }
+
+    /* Finish any remaining elems */
+    // TODO: figure out how to get rid of this special case to make good taste
+    //       code.
+    for (; i < length; i++)
+        acc0 = acc0 OP data[i];
+
+    *dest = acc0 OP acc1;
+}
