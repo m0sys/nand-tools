a97ed42332c4defcec4eaee5cfd004ef8cddb171
M0 <m0sys@users.noreply.github.com>
2022-04-05 06:41:30 -0400

    feat(csapp): add more locality pp

--- 03-textbooks/03-csapp/06-mem-hierarchy/locality.c
+++ 03-textbooks/03-csapp/06-mem-hierarchy/locality.c
@@ -34,3 +34,51 @@ int prod_arr3d(int a[N][N][N])
 
     return prod;
 }
+
+// PP 6.8
+
+#define N 1000
+typedef struct {
+    int vel[3];
+    int acc[3];
+} point;
+
+void clear1(point* p, int n)
+{
+    // This one has the highest spacial locality since each inner loop will have
+    // to load on chunk of memory each. i.e. the first one will load in vel,
+    // the second one will load acc.
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < 3; j++)
+            p[i].vel[j] = 0;
+        for (j = 0; j < 3; j++)
+            p[i].acc[j] = 0;
+    }
+}
+
+void clear2(point* p, int n)
+{
+    // This one has the second highest spacial locality since we only have to
+    // reload vel, acc on each inner iteration.
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < 3; j++) {
+            p[i].vel[j] = 0;
+            p[i].acc[j] = 0;
+        }
+    }
+}
+
+void clear3(point* p, int n)
+{
+    // This one has the lowest spacial locality since p[i] has to get loaded
+    // each time.
+    int i, j;
+    for (j = 0; j < 3; j++) {
+        for (i = 0; i < n; i++)
+            p[i].vel[j] = 0;
+        for (j = 0; j < 3; j++)
+            p[i].acc[j] = 0;
+    }
+}
