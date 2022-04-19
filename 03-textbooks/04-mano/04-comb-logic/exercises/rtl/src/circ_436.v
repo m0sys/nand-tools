4fa56869fa6aa86af42327af2f92fe7ba81e3da3
m0sys <m0sys.@users.noreply.github.com>
2022-04-19 05:30:41 -0400

    feat(mano): add some s4 slns

---
+++ 03-textbooks/04-mano/04-comb-logic/exercises/rtl/src/circ_436.v
@@ -0,0 +1,23 @@
+// Gate-lvl desc of 4bit priority encoder (Fig. 4.23).
+
+module circ_436(
+    // OUTPUTS
+    output y
+    ,output x
+    ,output V
+
+    // INPUTS
+    ,input D[3:0]
+    );
+
+    wire D2_b;
+    wire w0;
+
+    not (D2_b, D[2]);
+    and (w0, D2_b, D[1]);
+
+    or (x, D[3], D[2]);
+    or (V, x, D[1], D[0]);
+
+    or (y, D[3], w0);
+endmodule
---
+++ 03-textbooks/04-mano/04-comb-logic/exercises/rtl/src/circ_437.v
@@ -0,0 +1,4 @@
+// Gate-lvl desc of 4bit add-sub circuit (Fig. 4.13).
+
+module circ_437
+endmodule
