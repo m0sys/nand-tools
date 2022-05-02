bf51efaee0493bde19eba5418ce20057961a7ba9
m0sys <21181457+m0sys@users.noreply.github.com>
2022-05-02 18:56:56 -0400

    feat(zip): add maskbus impl + sim

--- 02-tutorials/01-zcpu/01-verilog-intro/01-wires-comb-logic/bash/compile_thruwire.sh
+++ 02-tutorials/01-zcpu/01-verilog-intro/01-wires-comb-logic/bash/compile_thruwire.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+
+cd rtl/src
+
+verilator -Wall -cc thruwire.v
+cd obj_dir
+make -f Vthruwire.mk
+
+cd ../../../
+
 g++-11 -I /opt/homebrew/Cellar/verilator/4.220/share/verilator/include -I rtl/src/obj_dir /opt/homebrew/Cellar/verilator/4.220/share/verilator/include/verilated.cpp cc/thruwire.cc rtl/src/obj_dir/Vthruwire__ALL.a -o thruwire
 
