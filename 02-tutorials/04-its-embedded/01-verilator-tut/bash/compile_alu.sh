0ccfae001d2c684a8f1d464b3f86a3f53f92a0a3
m0sys <21181457+m0sys@users.noreply.github.com>
2022-05-06 07:57:38 -0400

    chore(tut): update sh

--- 02-tutorials/04-its-embedded/01-verilator-tut/bash/compile_alu.sh
+++ 02-tutorials/04-its-embedded/01-verilator-tut/bash/compile_alu.sh
@@ -3,6 +3,7 @@ rm -rf rtl/src/obj_dir
 cd rtl/src
 
 
+# This command delegates compilation to Makefile.
 verilator -Wall --trace  -cc alu.sv --exe ../../cc/tb_alu.cc
 
 make -C obj_dir -f Valu.mk Valu
