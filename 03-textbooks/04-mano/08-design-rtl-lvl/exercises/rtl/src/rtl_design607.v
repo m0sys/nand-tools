92cbd950ee736359ee06daf046692f5b828e7614
m0sys <21181457+m0sys@users.noreply.github.com>
2022-06-09 07:33:44 -0400

    feat: add controller for rtl design 807

---
+++ 03-textbooks/04-mano/08-design-rtl-lvl/exercises/rtl/src/rtl_design607.v
@@ -0,0 +1,13 @@
+// Top module for rtl design exercise 8.7.
+
+module rtl_design607 #(parameter WIDTH=8)  (
+    // OUTPUTS
+    output [WIDTH-1:0] RA_o
+    ,output carry_o
+
+    // INPUTS
+    ,input start_i
+    ,input clk_i
+    ,input rst_b_i
+    );
+endmodule
